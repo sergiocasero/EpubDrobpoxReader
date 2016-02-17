765748e5ae6f38288bf91ecfbc5b233eec536ee0
Sergio Casero Hernández <sergio.casero23@gmail.com>
2016-02-17 20:53:09 +0100

    Added pub reader and epub downloader async task

--- app/src/main/java/com/sergiocasero/epubdrobpoxreader/activity/ListActivity.java
+++ app/src/main/java/com/sergiocasero/epubdrobpoxreader/activity/ListActivity.java
@@ -3,7 +3,10 @@ package com.sergiocasero.epubdrobpoxreader.activity;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.dropbox.client2.DropboxAPI;
@@ -13,17 +16,38 @@ import com.dropbox.client2.session.AppKeyPair;
 import com.sergiocasero.epubdrobpoxreader.R;
 import com.sergiocasero.epubdrobpoxreader.util.Util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import nl.siegmann.epublib.domain.Author;
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.epub.EpubReader;
 
 public class ListActivity extends AppCompatActivity {
 
     private static final String TAG = "ListActivity";
+
     private DropboxAPI<AndroidAuthSession> dropboxAPI;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Bind(R.id.navigation)
+    NavigationView navigationView;
+
+    @Bind(R.id.book_list)
+    RecyclerView bookList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        ButterKnife.bind(this);
 
         initDropboxApi();
         getEpubNames();
@@ -46,6 +70,14 @@ public class ListActivity extends AppCompatActivity {
         retriever.execute();
     }
 
+    public void getBooks(List<String> fileNames) {
+        DownloadBook downloadBook;
+        for (String fileName : fileNames) {
+            downloadBook = new DownloadBook();
+            downloadBook.execute(fileName);
+        }
+    }
+
     private class FilesRetriever extends AsyncTask<Void, Void, List<DropboxAPI.Entry>> {
 
 
@@ -61,9 +93,36 @@ public class ListActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(List<DropboxAPI.Entry> result) {
+            List<String> fileNames = new ArrayList<>();
             for (DropboxAPI.Entry entry : result) {
-                Log.i(TAG, entry.fileName());
+                fileNames.add(entry.path);
             }
+
+            
+            getBooks(fileNames);
+        }
+    }
+
+    private class DownloadBook extends AsyncTask<String, Void, Book> {
+
+        @Override
+        protected Book doInBackground(String... params) {
+            try {
+                InputStream fileStream = dropboxAPI.getFileStream(params[0], "");
+                EpubReader epubReader = new EpubReader();
+                return epubReader.readEpub(fileStream);
+            } catch (DropboxException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Book book) {
+            List<Author> authors = book.getMetadata().getAuthors();
+            Log.i(TAG, book.getTitle() + "- " + authors.toString());
         }
     }
 }
